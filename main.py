1c42b5d194b1822ff35d28f66af02ad15e7a0c1d
Julian <timbsjulian@gmail.com>
2023-12-12 21:28:12 +1000

    Added basic ability to play game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 import random
 
+class InvalidInputError(Exception):
+    pass
+
 card_value = {
     'Ace': 11,
     'King': 10,
@@ -18,7 +21,7 @@ card_value = {
     '1': 1
 }
 
-user_hand = [10, 10, 10]
+user_hand = []
 dealer_hand = []
 
 def deal_card(hand):
@@ -35,8 +38,46 @@ def check_bust(list):
     for i in list:
         total += i
     if total > 21:
-        return "Bust"
-    else:
-        return "Under 21"
+        return True
+
+def play_game():
+    print("The dealer is dealing your cards...")
+    while len(user_hand) < 2:
+        deal_card(user_hand)
+    print(f"Your hand is: {user_hand}")
+    print("The dealer is dealer is dealing their card...")
+    deal_card(dealer_hand)
+    print(f"The dealer's hand: {dealer_hand}\nYour hand: {user_hand}")
     
-print(check_bust(user_hand))
+    while True:    
+        try:
+            user_input = input("Press 'h' to hit or 's' to stand: ").lower()
+            if user_input not in ('h', 's', 'q'):
+                raise InvalidInputError("Invalid input")
+        except InvalidInputError as e:
+            print(e)
+        if user_input == "h":
+            deal_card(user_hand)
+            print(f"The dealer's hand: {dealer_hand}")
+            print(f"Your hand: {user_hand}")
+        check_bust(user_hand)
+        if check_bust(user_hand):
+            print("Bust!")
+            return
+        if check_bust(user_hand) != "bust" or user_input == "s":
+            print("The dealer is dealing their next card...")
+            deal_card(dealer_hand)
+            print(f"The dealer's hand: {dealer_hand}\nYour hand: {user_hand}")
+        check_bust(dealer_hand)
+        if check_bust(dealer_hand) == True:
+            print("You win!")
+        if user_input == "q":
+            exit()
+
+def help():
+    pass
+
+def main():
+    pass
+
+play_game()
